3afe907ed4ae3ff7297b3fd78b09b45a5f44d8c7
Aaron Cordovez <aaron.cordovez.m@gmail.com>
2014-12-23 16:12:08 -0800

    Add Towers of Hanoi, Browser version. Move files into directories

--- makenames/namesfrom.js
+++
@@ -1,7 +0,0 @@
-var namesAry = ["Daenerys", "Joffrey", "Tyrion", "Jon", "Arya", "Gregor", "Sansa",
-"Theon", "Cersei", "Jaime", "Drogo", "Eddard", "Margaery", "Robb", "Bran", "Tywin"]
-
-function newName( j ) {
-  var randomNum = Math.floor(Math.random()*namesAry.length);
-  j.text( namesAry[randomNum] );
-}
